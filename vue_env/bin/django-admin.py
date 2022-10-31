513e946b2532b61ea15c3cc5c5039e3d97082d40
dsledge524 <80835514+dsledge524@users.noreply.github.com>
2022-10-30 19:06:18 -0500

    assignment 4 commit

---
+++ vue_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dsledge/github-classroom/uno-isqa-4900/assignment4-dsledge524/vue_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
